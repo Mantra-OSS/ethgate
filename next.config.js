e391ec4fabfa71b35490e1b02d702ef97b65bbf2
JC <jc@pnkr.io>
2023-11-03 22:08:27 +0300

    fixes

--- next.config.js
+++ next.config.js
@@ -5,6 +5,12 @@ const nextConfig = {
   // Configure `pageExtensions`` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
+  poweredByHeader: false,
+  i18n: {
+    locales: ["en-US", "tr-TR"],
+    defaultLocale: "en-US",
+    localeDetection: false,
+  },
 };
 
 module.exports = withMDX(nextConfig);
