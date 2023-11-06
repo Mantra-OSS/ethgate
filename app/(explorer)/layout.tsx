8a065259edb534209e23dad215a59cf88a9a0b0a
JC <jc@pnkr.io>
2023-11-06 21:01:25 +0300

    asd

--- app/(explorer)/layout.tsx
+++ app/(explorer)/layout.tsx
@@ -1,14 +1,12 @@
 'use server';
 import { GitHub, Twitter } from '@mui/icons-material';
-import { Alert, AppBar, Box, Button, IconButton, Stack, Toolbar } from '@mui/material';
+import { Alert, AppBar, Box, Button, IconButton, Paper, Stack, Toolbar } from '@mui/material';
 import type { Metadata, Viewport } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
 
 import ClientProvider from '../client/AppProvider';
 import logo from '../client/logo.svg';
-
-import AppFooter from './AppFooter';
 
 // https://nextjs.org/docs/app/api-reference/functions/generate-metadata
 export async function generateMetadata(): Promise<Metadata> {
@@ -62,7 +60,18 @@ export default async function ExplorerLayout({
         </AppBar>
         {children}
       </Stack>
-      <AppFooter />
+      <Box m={1} mt="auto">
+        <Paper>
+          <Stack width="100%" direction="row" justifyContent="flex-end" padding={2}>
+            <IconButton href="https://github.com/mantra-oss/" target="_blank" color={'primary'}>
+              <GitHub />
+            </IconButton>
+            <IconButton href="https://twitter.com/ethgate_io" target="_blank" color={'primary'}>
+              <Twitter />
+            </IconButton>
+          </Stack>
+        </Paper>
+      </Box>
     </ClientProvider>
   );
 }
