c946217fa871309a3851f276792c27268c3869d5
JC <jc@pnkr.io>
2023-11-06 22:37:58 +0300

    asd

--- app/components/ChainView.tsx
+++ app/components/ChainView.tsx
@@ -9,6 +9,7 @@ import { FallbackBoundary } from '../components/ui';
 import ChainBlockList from './ChainBlockList';
 import ChainOverview from './ChainOverview';
 import ChainTransactionList from './ChainTransactionList';
+import { NodePageSection } from './NodePage';
 import NodePageBarContent from './NodePageBarContent';
 
 export default function ChainView({ node }: { node: Chain }) {
@@ -26,27 +27,26 @@ export default function ChainView({ node }: { node: Chain }) {
         </Paper>
       </Grid>
       <Grid item xs={12} md={6}>
-        <Paper>
-          <Stack direction="row" alignItems="center" px={1} py={0.5}>
-            <Typography variant="h3" pl={1} flex={1}>
-              Blocks
-            </Typography>
-            <Tooltip title="View All">
-              <IconButton
-                href={`${node.data.chainId}/blocks`}
-                size="small"
-                aria-label="view all"
-                color="primary"
-              >
-                <ArrowOutward />
-              </IconButton>
-            </Tooltip>
-          </Stack>
-          <Divider />
-          <FallbackBoundary>
-            <ChainBlockList chainId={node.id} />
-          </FallbackBoundary>
-        </Paper>
+        <NodePageSection
+          node={node}
+          title="Blocks"
+          actions={
+            <>
+              <Tooltip title="View All">
+                <IconButton
+                  href={`${node.data.chainId}/blocks`}
+                  size="small"
+                  aria-label="view all"
+                  color="primary"
+                >
+                  <ArrowOutward />
+                </IconButton>
+              </Tooltip>
+            </>
+          }
+        >
+          <ChainBlockList chainId={node.id} />
+        </NodePageSection>
       </Grid>
       <Grid item xs={12} md={6}>
         <Paper>
--- app/components/NodePage.tsx
+++ app/components/NodePage.tsx
@@ -1,5 +1,53 @@
+import { Divider, Paper, Stack, Typography } from '@mui/material';
+
 import type { SolverNode } from '../../solver/data';
+
+import { FallbackBoundary } from './ui';
 
 export function NodePage({ node, children }: { node: SolverNode; children: React.ReactNode }) {
   return <>{children}</>;
+}
+
+export function NodePageSection({
+  node,
+  title,
+  actions,
+  children,
+}: {
+  node: SolverNode;
+  title: string;
+  actions?: React.ReactNode;
+  children: React.ReactNode;
+}) {
+  return (
+    <Paper>
+      {children}
+    </Paper>
+  );
+}
+
+
+export function NodePageConnectionSection({
+  node,
+  title,
+  actions,
+  children,
+}: {
+  node: SolverNode;
+  title: string;
+  actions?: React.ReactNode;
+  children: React.ReactNode;
+}) {
+  return (
+    <Paper>
+      <Stack direction="row" alignItems="center" px={1} py={0.5}>
+        <Typography variant="h3" pl={1} flex={1}>
+          {title}
+        </Typography>
+        {actions}
+      </Stack>
+      <Divider />
+      <FallbackBoundary>{children}</FallbackBoundary>
+    </Paper>
+  );
 }
