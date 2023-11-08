ff84b54c0149af8e68bfc5cda4df083a38568410
JC <jc@pnkr.io>
2023-11-08 20:51:46 +0300

    asd

--- app/components/NodeConnectionPage.tsx
+++ app/components/NodeConnectionPage.tsx
@@ -1,12 +1,17 @@
 'use client';
 
-import type { SolverNode } from '@/lib-solver';
-import { Grid, Paper, Stack } from '@mui/material';
+import type { EdgeType, SolverNode } from '@/lib-solver';
+import { ArrowOutward } from '@mui/icons-material';
+import { Box, Divider, Grid, IconButton, Paper, Stack, Tooltip } from '@mui/material';
+import { FormattedMessage } from 'react-intl';
 
 import { useSolver } from '../client/backend';
 
-import { NodePageConnectionSection } from './NodePage';
+import NodeConnectionList from './NodeConnectionList';
+import { NodePageSection } from './NodePage';
 import NodePageBarContent from './NodePageBarContent';
+import { BlockListItem, ChainListItem, LogListItem, TransactionListItem } from './list-items';
+import { FallbackBoundary } from './ui';
 
 // import ChainBlockList from './ChainBlockList';
 
@@ -29,8 +34,95 @@ export default function NodeConnectionPage({
         </Paper>
       </Grid>
       <Grid item xs={12}>
-        <NodePageConnectionSection node={node} edgeType={edgeType} />
+        <NodeConnectionPageOverview node={node} edgeType={edgeType} />
       </Grid>
     </Grid>
+  );
+}
+
+export function NodeConnectionPageOverview({
+  node,
+  edgeType,
+}: {
+  node: SolverNode;
+  edgeType: EdgeType<any>;
+}) {
+  let renderItem: React.ComponentProps<typeof NodeConnectionList>['renderItem'];
+  switch (edgeType.name) {
+    case 'ChainHasChain': {
+      renderItem = ({ headId }) => <ChainListItem chainId={headId} />;
+      break;
+    }
+    case 'ChainHasDescendantBlock': {
+      renderItem = ({ headId }) => <BlockListItem blockId={headId} />;
+      break;
+    }
+    case 'ChainHasBlock': {
+      renderItem = ({ headId }) => <BlockListItem blockId={headId} />;
+      break;
+    }
+    case 'ChainHasTransaction': {
+      renderItem = ({ headId }) => <TransactionListItem transactionId={headId} />;
+      break;
+    }
+    case 'BlockHasTransaction': {
+      renderItem = ({ headId }) => <TransactionListItem transactionId={headId} />;
+      break;
+    }
+    case 'BlockHasLog': {
+      renderItem = ({ headId }) => <LogListItem logId={headId} />;
+      break;
+    }
+    case 'TransactionHasLog': {
+      renderItem = ({ headId }) => <LogListItem logId={headId} />;
+      break;
+    }
+    default:
+      throw new Error(`Unknown edge type: ${edgeType.name}`);
+  }
+  return (
+    <NodePageSection
+      title={
+        <FormattedMessage
+          id={`Connection.title`}
+          defaultMessage={`{name, select,
+            chains {Chains}
+            blocks {Blocks}
+            transactions {Transactions}
+            receipts {Receipts}
+            logs {Logs}
+            other {Connection "{name}"}
+          }`}
+          values={{
+            name: edgeType.connectionName,
+          }}
+        />
+      }
+      actions={
+        <>
+          <Tooltip title={<FormattedMessage id="Connection.viewAll" defaultMessage="View All" />}>
+            <IconButton
+              href={`${node.meta.slug}/${edgeType.connectionName}`}
+              size="small"
+              color="primary"
+            >
+              <ArrowOutward />
+            </IconButton>
+          </Tooltip>
+        </>
+      }
+    >
+      <Stack
+        style={{
+          overflowY: 'auto',
+        }}
+      >
+        <FallbackBoundary>
+          <NodeConnectionList node={node} edgeType={edgeType} renderItem={renderItem} />
+        </FallbackBoundary>
+      </Stack>
+      <Divider />
+      <Box pt={1} />
+    </NodePageSection>
   );
 }
