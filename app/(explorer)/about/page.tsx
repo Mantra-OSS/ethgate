5273ab5230dfa65461ba1a0a4ba6abf089ba398f
JC <jc@pnkr.io>
2023-11-08 20:45:03 +0300

    asd

--- app/(explorer)/about/page.tsx
+++ app/(explorer)/about/page.tsx
@@ -11,47 +11,66 @@ export default function AboutPage() {
   });
   return (
     <ExplorerLayout nav={<AppBreadcrumbs />}>
-      <Container maxWidth="xl">
-        <div>
-          <div>
-            <Typography variant="h3" fontWeight={500} padding={1} textAlign="center">
-              {' '}
-              About ETHgate.io{' '}
-            </Typography>
-          </div>
-          <div>
-            <EthgateLogo width={100} height={100} color="red" isLoading />
-          </div>
-          <div>
-            <Typography variant="h5" fontWeight={300} padding={1} textAlign="center">
-              {' '}
-              ETHgate is your one-stop block explorer for everything Ethereum, designed to make
-              complex blockchain data easy to access and understand. We take the complexity out of
-              exploring different blockchains by offering a unified platform that is intuitive to
-              use.{' '}
-            </Typography>
-          </div>
-          <div>
-            <Typography variant="h5" fontWeight={300} padding={1} textAlign="center">
-              {' '}
-              Our core aim is to democratize data access across EVM-compatible networks. Owing to
-              its flexible and modular nature, ETHgate works simply and seamlessly: no servers, no
-              hassle, just straight-up data at your fingertips.{' '}
-            </Typography>{' '}
-          </div>
-          <div>
-            <Typography variant="h5" fontWeight={300} padding={1} textAlign="center">
-              {' '}
-              ETHgate was built with a commitment to give users a straightforward tool for diving
-              into blockchain data. Whether you are a newcomer or a seasoned expert, ETHgate puts
-              the power of blockchain exploration right in your hands, all with just a few clicks.{' '}
-            </Typography>
-          </div>
-          <div>
-            <Image src="/qr" width={128} height={128} alt="QR code for ethgate.io" />
-            {/* <Image src="/image" width={1200} height={630} alt="ethgate.io" /> */}
-          </div>
-        </div>
+      <Container maxWidth="sm">
+        <Typography variant="h3" padding={1} textAlign="center">
+          About ethgate.io
+        </Typography>
+        <Typography variant="body1" padding={1} textAlign="center">
+          ethgate.io is your one-stop multichain block explorer. Flexible and modular in nature,
+          ethgate.io works simply and seamlessly: no servers, no hassle, just straight-up data at
+          your fingertips.
+        </Typography>
+      </Container>
+      <Container maxWidth="sm">
+        <Typography variant="h3" padding={1} textAlign="center">
+          Multichain
+        </Typography>
+        <Typography variant="body1" padding={1} textAlign="center">
+          chainlist here
+        </Typography>
+      </Container>
+      <Container maxWidth="sm">
+        <Typography variant="h3" padding={1} textAlign="center">
+          Realtime
+        </Typography>
+        <Typography variant="body1" padding={1} textAlign="center">
+          realtime stuff
+        </Typography>
+      </Container>
+      <Container maxWidth="sm">
+        <Typography variant="h3" padding={1} textAlign="center">
+          Responsive
+        </Typography>
+        <Typography variant="body1" padding={1} textAlign="center">
+          navigation stuff
+        </Typography>
+      </Container>
+      <Container maxWidth="sm">
+        <Typography variant="h3" padding={1} textAlign="center">
+          Private/Serverless
+        </Typography>
+        <Typography variant="body1" padding={1} textAlign="center">
+          navigation stuff
+        </Typography>
+      </Container>
+      <Container maxWidth="sm">
+        <Typography variant="h3" padding={1} textAlign="center">
+          Extensible
+        </Typography>
+        <Typography variant="body1" padding={1} textAlign="center">
+          twitter github
+          <Image src="/qr" width={128} height={128} alt="QR code for ethgate.io" />
+        </Typography>
+      </Container>
+      <Container maxWidth="sm">
+        <Typography variant="h3" padding={1} textAlign="center">
+          Contact us
+        </Typography>
+        <Typography variant="body1" padding={1} textAlign="center">
+          twitter github form on the websitre telegram
+          <Image src="/qr" width={128} height={128} alt="QR code for ethgate.io" />
+          <EthgateLogo width={100} height={100} color="red" isLoading />
+        </Typography>
       </Container>
     </ExplorerLayout>
   );
