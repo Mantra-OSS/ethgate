c89579b614d3b5ef00aba009ad93d9ab5a4b1ca5
JC <jc@pnkr.io>
2023-11-09 17:41:12 +0300

    Disable HomePage

--- app/(explorer)/(home)/page.tsx
+++ app/(explorer)/(home)/page.tsx
@@ -1,12 +1,12 @@
 import { AppBreadcrumbs } from '@/app/client/breadcrumbs';
 
-import HomeView from '../../components/HomeView';
+// import HomeView from '../../components/HomeView';
 import ExplorerLayout from '../ExplorerLayout';
 
 export default function HomePage() {
   return (
     <ExplorerLayout themeOptions={{}} nav={<AppBreadcrumbs />}>
-      <HomeView />
+      {/* <HomeView /> */}
     </ExplorerLayout>
   );
 }
